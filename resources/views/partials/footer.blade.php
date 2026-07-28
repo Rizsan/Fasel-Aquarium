cc9db9b02236f3bfb8b0226a13e258b70f08a72e
Rizky-san <mohamadrizkyramadhani17@gmail.com>
2026-07-28 09:03:51 +0700

    penyesuaian

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -75,6 +75,9 @@
                     <li>
     <a href="{{ route('terms') }}"
        class="text-gray-400 hover:text-white transition flex items-center gap-2">
+       <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
+                            </svg>
         Syarat & Ketentuan
     </a>
 </li>
@@ -82,6 +85,9 @@
 <li>
     <a href="{{ route('privacy') }}"
        class="text-gray-400 hover:text-white transition flex items-center gap-2">
+       <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
+                            </svg>
         Kebijakan Privasi
     </a>
 </li>
