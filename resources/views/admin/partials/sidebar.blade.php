c619ae848ec71fc6914805271fc9863ff5673993
Rizky-san <mohamadrizkyramadhani17@gmail.com>
2026-08-31 19:38:29 +0700

    last update

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -77,14 +77,6 @@
 
                 [
                     'icon' =>
-                        'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z',
-                    'label' => 'Mortality Ikan',
-                    'route' => 'admin.mortality.index',
-                    'active' => request()->routeIs('admin.mortality.*'),
-                ],
-                
-                [
-                    'icon' =>
                         'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197',
                     'label' => 'Pengguna',
                     'route' => 'admin.users.index',
